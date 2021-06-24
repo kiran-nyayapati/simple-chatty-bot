3c87df6bfa8e5552c114797c6872c463d2208c6a
Kiran Nyayapati <kiran.nyayapati@gmail.com>
2021-06-24 23:37:48 +0530

    minor changes in printing data section

--- Topics/Printing data/Digits from 0 to 9 in a single line/src/Main.java
+++ Topics/Printing data/Digits from 0 to 9 in a single line/src/Main.java
@@ -1,9 +1,12 @@
 class Main {
     public static void main(String[] args) {
-       /*for (int i = 9; i >= 0; i--) {
-            System.out.print(i + " ");
-       }
-       System.out.print("\b");*/
-       System.out.print("9 8 7 6 5 4 3 2 1 0");
+       /*
+           for (int i = 9; i >= 0; i--) {
+                System.out.print(i + " ");
+           }
+           System.out.print("\b");
+       */
+        String numbers = "9 8 7 6 5 4 3 2 1 0";
+        System.out.print(numbers);
     }
 }
